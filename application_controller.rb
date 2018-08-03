1b54014188da7ca430d2802ad595e9eefbc5bf99
hanam0822 <hanam0822@gmail.com>
2018-08-03 13:15:48 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,21 @@
 require 'bundler'
+require './models/model'
 Bundler.require
 
-class MyApp < Sinatra::Base
-
-  get '/' do
-    erb :index
-  end
-
+class Application < Sinatra::Base
+ 
+ get '/specialeducation' do
+  "Special Education"
+  erb:specialeducation
+ end
+ 
+ get '/aboutus' do
+   "About Us"
+   erb :aboutus
+ end 
+  
+# post '/' do
+#	"I am #{params[:name]}, and I would like a #{params[:espresso]} shot of espresso , with #{params [:milk]} milk and #{params [:sugar]} packets of sugar!"
+ #end
+ 
 end
